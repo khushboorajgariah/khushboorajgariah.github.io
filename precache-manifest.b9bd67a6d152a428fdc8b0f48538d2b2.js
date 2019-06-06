dc1259ef128b94eb2ca24fdb9c0f1c034af5e0fe
khushboorajgariah <khushboo.rajgariah@shuttl.com>
2019-06-06 14:40:23 +0530

    Move build files to root

---
+++ precache-manifest.b9bd67a6d152a428fdc8b0f48538d2b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e5d3f4fddccd806bed3c",
+    "url": "/static/js/main.4ef84c89.chunk.js"
+  },
+  {
+    "revision": "2b59cc6805cfd521df50",
+    "url": "/static/js/2.19ae50ac.chunk.js"
+  },
+  {
+    "revision": "e5d3f4fddccd806bed3c",
+    "url": "/static/css/main.d8ed24c3.chunk.css"
+  },
+  {
+    "revision": "feabdbd5db64404b086a92fa20ec1bb2",
+    "url": "/index.html"
+  }
+];
